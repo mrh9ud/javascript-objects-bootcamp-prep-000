91ba33a8a58452b36263d8afc501f6044022703a
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-01 15:21:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,12 @@
 var playlist = {'artist': 'songTitle'};
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
+
 function updatePlaylist(playlist, artist, songTitle) {
   playlist[artist] = songTitle
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
-  return playlist
-}
